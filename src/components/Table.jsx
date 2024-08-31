cc1ae038ff836104c17b5045957db139de93177e
rahinasrahman <rshinasrahman94@gmail.com>
2024-08-30 23:13:54 -0500

    react part 3

--- src/components/Table.jsx
+++ src/components/Table.jsx
@@ -1,17 +1,42 @@
+import "./styling.css";
 const Table = ({ sat }) => {
   return (
     <table>
-    <thead>
-     <tr>
-       <th>Header TBD</th>
-     </tr>
-     </thead>
-     <tbody>
-     <tr>
-       <td>Row Data TBD</td>
-     </tr>
-     </tbody>
-   </table>
+      <thead>
+        <tr>
+          <th>
+            <td>Name</td>
+          </th>
+          <th>
+            <td>Type of Satellite</td>
+          </th>
+          <th>
+            <td>Launch Date</td>
+          </th>
+          <th>
+            <td>Status</td>
+          </th>
+        </tr>
+      </thead>
+      <tbody>
+        {sat.map((data) => {
+          return (
+            <tr key={data.id}>
+              <td> {data.name}</td>
+              <td> {data.type} </td>
+              <td> {data.launchDate}</td>
+              <td>
+                {data.operational === true ? (
+                  <span style={{ color: "green" }}>Active</span>
+                ) : (
+                  <span style={{ color: "red" }}>Inactive</span>
+                )}
+              </td>
+            </tr>
+          );
+        })}
+      </tbody>
+    </table>
   );
 };
 
